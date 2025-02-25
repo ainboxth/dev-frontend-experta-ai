ee8ea8af910f30a3743168150c7fba1097679ccb
Mr.Pattanasak <111237717+PattanasakGit@users.noreply.github.com>
2025-02-26 01:58:12 +0700

    [FIX] update test dev backend

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,5 +5,5 @@ export const ConfigPage = {
 
   //============== production =====================
   URL_WORK_IMG: "src/images/img2img",
-  URL_BACKEND: "http://localhost:3000",
+  URL_BACKEND: "https://dev-frontend-experta-ai.vercel.app",
 };
