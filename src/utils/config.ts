eefa8e12100383ee8957314fe367e62729b57bfe
Mr.Pattanasak <111237717+PattanasakGit@users.noreply.github.com>
2025-02-27 23:07:50 +0700

    [FIX] use env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,12 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const ConfigPage = {
   //============== Development ================
   URL_WORK_IMG: `src/images/img2img`,
-  URL_BACKEND: "http://localhost:3000",
-  // URL_BACKEND: "https://9e61-2001-44c8-4033-3b79-30d9-91d1-a77-727b.ngrok-free.app",
-
-  //============== production =====================
-//   URL_WORK_IMG: "src/images/img2img",
-//   URL_BACKEND: "https://dev-frontend-experta-ai.vercel.app",
-
-
+  URL_BACKEND: process.env.URL_BACKEND || "http://localhost:3000",
 };
